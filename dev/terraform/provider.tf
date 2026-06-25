6f5d1d0914c1c68b15d82b66b4c5057f1c96d268
Rick Pohly <rickpohly@gmail.com>
2026-06-25 17:40:50 -0400

    Removed token

--- dev/terraform/provider.tf
+++ dev/terraform/provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "proxmox" {
   endpoint  = "https://pve-node-01.dev.local:8006/"
-  api_token = "terraform@pve!terratoken=703e49c8-93b2-43ab-ba46-c5e7a7f661c3"
+  api_token = "add_token_here"
   insecure = true
 }
